215349f3fc09ec77771382b6bf8f712ed2d8ccb6
adv-ait <advait.yogaonkar@leftrightmind.com>
2023-09-07 08:19:45 +0530

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import names from "./Names.json";
+import { useUpdateEffect } from "@react-hookz/web";
+import "animate.css";
+
+const App = () => {
+  // console.log(names[Math.floor(Math.random() * names.length)]);
+  const [isRendering, setisRendering] = useState(false);
+  const [name, setname] = useState(() => {
+    return names[Math.floor(Math.random() * names.length)];
+  });
+
+  const INTERVAL = 80;
+
+  const startRendering = () => {
+    setisRendering(true);
+    setTimeout(() => {
+      setisRendering(false);
+    }, 2000);
+  };
+
+  useUpdateEffect(() => {
+    console.log(`Clicked`);
+    if (isRendering) {
+      const nameInterval = setInterval(() => {
+        setname(names[Math.floor(Math.random() * names.length)]);
+      }, INTERVAL);
+
+      return () => clearInterval(nameInterval);
+    }
+  }, [isRendering]);
+
+  return (
+    <div className="App animate__animated animate__zoomInUp">
+      <h1 className="title">Host for The Standup</h1>
+      <div className="nameContainer">
+        <h1 onClick={() => startRendering()} className="names">
+          {name}
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default App;
